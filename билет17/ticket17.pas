86d1c45d33163b6a0ca44d84c0febaf479961979
gogun <gusarov2000@gmail.com>
2019-01-20 13:46:21 +0300

    Update ticket17.pas

--- билет17/ticket17.pas
+++ билет17/ticket17.pas
@@ -1,149 +1,138 @@
-﻿type
-  t_info = record
-    numb:integer;
-    nas:string[13];
+Program p11;
+type tr = ^tree;
+  tree = record
+    lf,rt:tr;
+    count:integer;
+    agekey:integer;
   end;
-  
-  sp = ^spisok;
-  spisok = record
-    numb:integer;
-    nas:string[13];
-    next:sp;
+
+  st = ^stak;
+  stak = record
+    data:tr;
+    k : boolean;
+    next:st;
   end;
 
 var
-  f: file of t_info;
-  f4: file of t_info;
-  f1,f2,f3:text;
-  uHead1,uHead2:sp;
-  
-  
-  procedure p1(var f:file of t_info; var f1:text);
-  var
-    info:t_info;
-    c:char;
-  begin
-    while not eof(f1) do
-    begin
-      readln(f1,info.numb,c,info.nas);
-      write(f,info);
-    end;
-  end;
-  
-  procedure p2(var f:file of t_info; var uHead1:sp);
-  var
-    tmp,u:sp;
-    info:t_info;
-  begin
-    while not eof(f) do
-    begin
-      new(u);
-      read(f,info);
-      u^.next:=nil;
-      u^.numb:=info.numb;
-      u^.nas:=info.nas;
-      tmp:=uHead1;
-      while tmp^.next <> nil do
-      begin
-        if u^.nas < tmp^.next^.nas then 
-          break;
-        tmp:=tmp^.next;
-      end;
-      if tmp^.next = nil then
-        tmp^.next:=u
-      else
-      begin
-        u^.next:=tmp^.next;
-        tmp^.next:=u;
-      end;
-    end;
-  end;
-  
-  procedure p4(var uHead1,uHead2:sp);
-  var
-    tmp1,tmp2,u:sp;
-  begin
-    tmp1:=uHead1;
-    tmp2:=uHead2^.next;
-    while tmp2 <> nil do
-      begin
-        tmp1:=uHead1;
-        while tmp1^.next <> nil do
-        begin
-          if tmp2^.nas < tmp1^.next^.nas  then 
-            break;
-          tmp1:=tmp1^.next;
-        end;
-        new(u);
-        u^.numb:=tmp2^.numb;
-        u^.nas:=tmp2^.nas;
-        
-        u^.next:=tmp1^.next;
-        tmp1^.next:=u;
-        
-        tmp2:=tmp2^.next;
-      end;
-   end;
-   
-  procedure p5(var uHead:sp;var f3:text);
-  var
-    tmp:sp;
-  begin
-    tmp:=uHead;
-    while tmp <> nil do 
-    begin
-      writeln(f3,tmp^.numb,' ',tmp^.nas);
-      tmp:=tmp^.next;
-    end;
-  end;
-  
-begin
-  assign(f,'res.tip');
-  rewrite(f);
-  assign(f1,'dan.txt');
-  reset(f1);
-  assign(f2,'dan1.txt');
-  reset(f2);
-  assign(f3,'res.txt');
-  rewrite(f3);
-  new(uHead1);
-  uHead1^.next:=nil;
-  new(uHead2);
-  uHead2^.next:=nil;
-  p1(f,f1);
-  close(f);
-  reset(f);
-  p2(f,uHead1);
-  close(f);
-  p5(uHead1^.next,f3);
-  writeln(f3);
-  
-  assign(f4,'res1.tip');
-  rewrite(f4);
-  p1(f4,f2);
-  close(f4);
-  reset(f4);
-  p2(f4,uHead2);
-  close(f4);
-  p5(uHead2^.next,f3);
-  
-  writeln(f3);
-  p4(uHead1,uHead2);
-  p5(uHead1^.next,f3);
-  close(f3);
-end.
-  
-  
-          
-        
-        
-     
-    
-    
+ uRoot:tr;
+ f1,f2:text;
+ age:integer;
+ k:integer;
 
-    
- 
-  
-    
-    
-  
+function fun(line:string):integer;
+var
+  err, i, age : integer;
+begin
+  for i := 1 to length(line) do
+    if (line[i] >= '0') and (line[i]<='9') then
+      break;
+  val(copy(line,i,length(line)-i+1), age, err);
+  fun:=age;
+end;
+
+procedure p1(var uRoot:tr; var f:text);
+label l1;
+var
+  dt,dt2:tr;
+  line:string;
+  age:integer;
+begin
+  new(uRoot);
+  readln(f,line);
+  age := fun (line);
+  uRoot^.agekey:=age;
+  uRoot^.lf:=nil;
+  uRoot^.rt:=nil;
+  uRoot^.count:=1;
+  l1:
+  while not eof(f) do
+  begin
+    dt:=uRoot;
+    readln(f,line);
+    age := fun (line);
+    while dt <> nil do
+    begin
+      dt2:=dt;
+      if age>dt^.agekey then
+        dt:=dt^.rt
+      else
+        if age < dt^.agekey then
+          dt:=dt^.lf
+        else begin
+          dt^.count := dt^.count + 1;
+          goto l1;
+        end;
+    end;
+    new(dt);
+    dt^.agekey:=age;
+    dt^.lf:=nil;
+    dt^.rt:=nil;
+    dt^.count := 1;
+    if age>dt2^.agekey then
+      dt2^.rt:=dt
+    else dt2^.lf:=dt;
+  end;
+end;
+
+procedure pp2(var u2, uHead : st; u : tr);
+  begin
+    new (u2);
+    u2^.next := uHead;
+    u2^.data := u;
+    uHead := u2;
+  end;
+
+procedure p2 (uRoot : tr; var f : text);
+  var uHead, u2 : st;
+      u: tr;
+      k : integer;
+  begin
+    new (uhead);
+    uHead^.data := uRoot;
+    uHead^.next := nil;
+    while uHead <> nil do begin
+      u := uHead^.data;
+      uHead := uHead^.next;
+      if u^.rt <> nil then
+        pp2 (u2, uHead, u^.rt);
+      if u^.lf <> nil then begin
+        pp2 (u2, uHead, u);
+        uHead^.k := true;
+        pp2 (u2, uHead, u^.lf);
+      end
+      else begin
+        write(f, u^.agekey, '-', u^.count, '; ');
+        while (uHead <> nil) and (uHead^.k = true) do begin
+          u := uHead^.data;
+          write(f, u^.agekey, '-', u^.count, '; ');
+          uHead := uHead^.next;
+        end;
+      end;
+    end;
+  end;
+
+procedure p3(u : tr;age:integer;var k:integer);
+begin
+    if u^.lf <> nil then
+      p3(u^.lf,age,k);
+    if u^.agekey<age then
+    k:=k+u^.count;
+    if u^.rt <> nil then
+      p3(u^.rt,age,k);
+end;
+
+Begin
+  assign (f1, 'inputT11.txt');
+  reset (f1);
+  assign (f2, 'outputT11.txt');
+  rewrite (f2);
+  uRoot := nil;
+  p1 (uRoot, f1);
+  p2 (uRoot, f2);
+  writeln('write age ');
+  readln(age);
+  p3(uRoot,age,k);
+  writeln(k);
+  close (f2);
+  readln;
+end.
